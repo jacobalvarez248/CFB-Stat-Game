6ff3cf05bba45c236c0a487c1190e99bf371392d
jacobalvarez248 <jacobalvarez248@gmail.com>
2025-07-16 09:24:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -169,7 +169,7 @@ if tab == "Standings":
                     titleFontSize=8,
                     symbolLimit=30,
                     symbolSize=30,
-                    columns=8
+                    columns=7
                 )
             ),
             order=alt.Order("week_idx:O"),
